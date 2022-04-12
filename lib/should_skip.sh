ad44463200b8143dda8a04f8ac660953b5a5eb91
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-04-12 16:21:38 +0200

    Use input value

--- lib/should_skip.sh
+++ lib/should_skip.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-SHOULD_SKIP=0
-if [[ "${VALID_WEEK_TYPE}" == "odd" || "${VALID_WEEK_TYPE}" == "even" ]]; then
+if [[ -z "${INPUT_VALID_WEEK_TYPE}" ]]; then
+  echo "Missing INPUT_VALID_WEEK_TYPE" >&2
+  exit 1
+fi
+
+SHOULD_SKIP=no
+if [[ "${INPUT_VALID_WEEK_TYPE}" == "odd" || "${INPUT_VALID_WEEK_TYPE}" == "even" ]]; then
   WEEK=`date +"%V"`
   if [ $(($WEEK%2)) -eq 0 ]; then
     CURRENT_WEEK_TYPE="even"
   else
     CURRENT_WEEK_TYPE="odd"
   fi
-  if [[ "${CURRENT_WEEK_TYPE}" != "${VALID_WEEK_TYPE}" ]]; then
-    SHOULD_SKIP=1
+  if [[ "${CURRENT_WEEK_TYPE}" != "${INPUT_VALID_WEEK_TYPE}" ]]; then
+    SHOULD_SKIP=yes
   fi
 fi
 echo "::set-output name=should_skip::${SHOULD_SKIP}"
