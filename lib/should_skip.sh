ddbd3ac7dfa3516f930223f3cc5b5534248c4744
Jan Mewes <jan.mewes@epages.com>
2023-02-23 10:54:42 +0100

    Fix octal number for week 08
    See https://stackoverflow.com/a/24777667/2339010

--- lib/should_skip.sh
+++ lib/should_skip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ -z "${INPUT_VALID_WEEK_TYPE}" ]]; then
   echo "Missing INPUT_VALID_WEEK_TYPE" >&2
   exit 1
@@ -8,7 +10,8 @@ fi
 SHOULD_SKIP=no
 if [[ "${INPUT_VALID_WEEK_TYPE}" == "odd" || "${INPUT_VALID_WEEK_TYPE}" == "even" ]]; then
   WEEK=`date +"%V"`
-  if [ $(($WEEK%2)) -eq 0 ]; then
+  WEEK=$((10#$WEEK)) # Use base 10 for the week number which may be e.g. "08"
+  if [[ $(($WEEK%2)) -eq 0 ]]; then
     CURRENT_WEEK_TYPE="even"
   else
     CURRENT_WEEK_TYPE="odd"
